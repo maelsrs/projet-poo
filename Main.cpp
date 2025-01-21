bbda676b4283d7c22d9e2bb4840e1041d3fdd773
smael <mael.sourisseau@ynov.com>
2025-01-21 11:10:54 +0100

    Added Main

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,12 @@
 ﻿#include "Main.hpp"
+#include <iostream>
+#include <string>
+#include <Windows.h>
+#include <cstdio>
+#include <memory>
+#include "Instrument.hpp"
+
+using namespace std;
 
 int main()
 {
